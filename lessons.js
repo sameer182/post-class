5a74170f87a9e0e6e30bce04301ced86b410ab5f
Sameer Limbu <sameerlimbu31@gmail.com>
2023-11-06 16:43:10 +0000

    Added lessons.js file, made crucial changes in functionlaity of website.

---
+++ lessons.js
@@ -0,0 +1,31 @@
+const lessons = [
+    {
+    id: 100,
+    title: "Computer Programming",
+    description: "A beginner's guide to our digital world",
+    location: "Hendon",
+    price: 200,
+    spaces: 5,
+    image: "pic/coding.png",
+    availableInventory: 5
+},
+{
+id: 101,
+    title: "Math",
+    description: "A beginner's guide to our digital world",
+    location: "Hendon",
+    price: 200,
+    image: "pic/coding.png",
+    availableInventory: 5
+},
+{
+id: 102,
+    title: "Science",
+    description: "A beginner's guide to our digital world",
+    location: "Hendon",
+    price: 200,
+    image: "pic/coding.png",
+    availableInventory: 5
+},
+];
+export default lessons;
