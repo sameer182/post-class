7a7e9e993ea05702df858a629d75855e64914bcb
Sameer Limbu <sameerlimbu31@gmail.com>
2023-11-07 00:21:10 +0000

    Version 2.0 Made a big updates - Added 10 lessons in JS file - Added sort function *still working* - Additional CSS styles - Additional pictures

--- lessons.js
+++ lessons.js
@@ -1,31 +1,85 @@
 const lessons = [
     {
-    id: 100,
-    title: "Computer Programming",
-    description: "A beginner's guide to our digital world",
-    location: "Hendon",
-    price: 200,
-    spaces: 5,
-    image: "pic/coding.png",
-    availableInventory: 5
-},
-{
-id: 101,
-    title: "Math",
-    description: "A beginner's guide to our digital world",
-    location: "Hendon",
-    price: 200,
-    image: "pic/coding.png",
-    availableInventory: 5
-},
-{
-id: 102,
-    title: "Science",
-    description: "A beginner's guide to our digital world",
-    location: "Hendon",
-    price: 200,
-    image: "pic/coding.png",
-    availableInventory: 5
-},
+        id: 100,
+        title: "Computer Programming",
+        location: "Hendon",
+        price: 100,
+        image: "pic/coding.png",
+        availableInventory: 5
+    },
+    {
+        id: 101,
+        title: "Math",
+        location: "America",
+        price: 300,
+        image: "pic/math.png",
+        availableInventory: 5
+    },
+    {
+        id: 102,
+        title: "Science",
+        location: "Nepal",
+        price: 200,
+        image: "pic/science.png",
+        availableInventory: 5
+    },
+    {
+        id: 103,
+        title: "Astronomy",
+        location: "UK",
+        price: 150,
+        image: "pic/astronomy.png",
+        availableInventory: 5
+    },
+    {
+        id: 104,
+        title: "Health",
+        location: "UK",
+        price: 150,
+        image: "pic/health.png",
+        availableInventory: 5
+    },
+    {
+        id: 105,
+        title: "Business",
+        location: "UK",
+        price: 150,
+        image: "pic/business.png",
+        availableInventory: 5
+    },
+    {
+        id: 106,
+        title: "Accounting",
+        location: "UK",
+        price: 150,
+        image: "pic/accounting.png",
+        availableInventory: 5
+    },
+    {
+        id: 107,
+        title: "Sports",
+        location: "UK",
+        price: 150,
+        image: "pic/sports.png",
+        availableInventory: 5
+    },
+    {
+        id: 108,
+        title: "Engineering",
+        location: "UK",
+        price: 150,
+        image: "pic/engineering.png",
+        availableInventory: 5
+    },
+    {
+        id: 109,
+        title: "Culinary",
+        location: "UK",
+        price: 150,
+        image: "pic/culinary.png",
+        availableInventory: 5
+    },
+   
+    
 ];
 export default lessons;
